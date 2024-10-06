d8a58c43533d5c15515be023ccfad904c820fef5
Bacem Karray <99637476+bacemkarray@users.noreply.github.com>
2024-10-06 13:41:45 -0400

    Update staLTATrigger.py

--- staLTATrigger.py
+++ staLTATrigger.py
@@ -1,15 +1,15 @@
 from obspy.core import read
-from obspy.signal.trigger import plot_trigger
-from obspy.signal.trigger import classic_sta_lta
-import math
+from plotTrigger import plot_trigger
+from obspy.signal.trigger import recursive_sta_lta
+
 
 data_directory = r'C:\Users\bkarr\Downloads\space_apps_2024_seismic_detection\space_apps_2024_seismic_detection\data\lunar\test\data\S12_GradeB'
-test_filename = r'\xa.s12.00.mhz.1970-02-18HR00_evid00016'
+test_filename = r'\xa.s12.00.mhz.1970-03-30HR00_evid00020'
 mseed_file = f'{data_directory}{test_filename}.mseed'
 
-number = 1.5 * math.pow(10,-9)
 trace = read(mseed_file)[0]
 df = trace.stats.sampling_rate
 
-cft = classic_sta_lta(trace.data, int(453 * df), int(1510* df))
-plot_trigger(trace, cft, 3.1, 2)
+
+cft = recursive_sta_lta(trace.data, int(453 * df), int(1510* df))
+trigger_values = plot_trigger(trace, cft, 2.6, 0.4)
